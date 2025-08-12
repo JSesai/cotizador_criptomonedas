f009c77d2341c49b37b1dd4db309b6b931b9a6d9
julio sesai <antonio.sesai@gmail.com>
2025-08-11 23:47:23 -0600

    agregando store de zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import CriptoSearchForm from "./components/CriptoSearchForm/CriptoSearchForm";
+import { useCryptoStore } from "./store/store";
 
 export default function App() {
+
+  const fetchCriptos = useCryptoStore((state) => state.fetchCriptos);
+
+  useEffect(()=>{
+    fetchCriptos()
+  },[]);
+  
   return (
     <div className="container">
       <h1 className="app-title">Cotizador de <span>criptomonedas</span> </h1>
--- src/schema/crypo-schema.ts
+++ src/schema/crypo-schema.ts
@@ -5,3 +5,21 @@ export const CurrencySchema = z.object({
     code: z.string(),
     name: z.string()
 })
+
+
+export const currencyResponse = z.object({
+    NAME: z.string(),
+    SYMBOL: z.string()
+})
+
+export const CryptoCurrencyResposeSchema = z.object({
+    Data: z.object({
+        LIST: z.array(
+            currencyResponse
+        )
+
+    })
+
+});
+
+export const CryptoCurrenciesResponseSchema = z.array(currencyResponse);
